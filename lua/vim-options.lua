d400cd11febdacf2e316596eebd9d84fdb2b8b3f
Matt Rothstein <matthewrothstein@hotmail.com>
2024-11-20 07:50:07 -0500

    first neovim config

---
+++ lua/vim-options.lua
@@ -0,0 +1,27 @@
+vim.cmd("set expandtab")
+vim.cmd("set tabstop=2")
+vim.cmd("set softtabstop=2")
+vim.cmd("set shiftwidth=2")
+vim.g.mapleader = " "
+vim.g.background = "light"
+
+vim.opt.swapfile = false
+
+-- Navigate vim panes better
+vim.keymap.set('n', '<c-k>', ':wincmd k<CR>')
+vim.keymap.set('n', '<c-j>', ':wincmd j<CR>')
+vim.keymap.set('n', '<c-h>', ':wincmd h<CR>')
+vim.keymap.set('n', '<c-l>', ':wincmd l<CR>')
+
+-- Key mappings
+vim.keymap.set("n", "<Up>", "<NOP>")
+vim.keymap.set("n", "<Down>", "<NOP>")
+vim.keymap.set("n", "<Left>", "<NOP>")
+vim.keymap.set("n", "<Right>", "<NOP>")
+
+vim.api.nvim_create_user_command("Q", "quit", { bang = true })
+
+vim.opt.number = true
+vim.opt.numberwidth = 5
+
+vim.fn.setenv('PATH', vim.fn.expand('/opt/homebrew/opt/ruby/bin:$PATH'))
